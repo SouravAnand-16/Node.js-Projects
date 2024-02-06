6a709bcea433c25b4732c040fd9b0704c9314a88
Sourav Anand <sauravanand324@gmail.com>
2024-02-06 17:42:13 +0530

    Created Movie Store supports filter/sort/paginate

--- MovieStore/Routes/movieRoutes.js
+++ MovieStore/Routes/movieRoutes.js
@@ -12,16 +12,21 @@ router.get("/",async(req,res)=>{
         if (req.query.rating) {
             query.rating = req.query.rating;
         }
-
+        if (req.query.q) {
+            query.title = { $regex: req.query.q, $options: 'i' };
+        }
+        const sortOption = req.query.sort || 'title' ;
+        const sortOrder = req.query.order === 'desc' ? -1 : 1 ;
         let movies;
         if (Object.keys(query).length === 0) {
-            movies = await MovieModel.find();
+            movies = await MovieModel.find().sort({[sortOption]:sortOrder});
         } else {
             console.log(query);
-            movies = await MovieModel.find(query);
+            movies = await MovieModel.find(query).sort({[sortOption]:sortOrder});
         }
         res.status(200).json({ message: "Here are the filtered movies:", movies });
     }catch(error){
+        console.error("Error fetching movies:", error);
         res.status(500).send("Cannot fetch Movie");
     }
 })
@@ -42,5 +47,17 @@ router.post("/",async(req,res)=>{
         res.status(500).send("Error while posting new movie");
     }
 
+})
+
+router.delete("/:id",async(req,res)=>{
+   try{
+        const {id} = req.params ;
+        console.log(id);
+        await MovieModel.findByIdAndDelete(id);
+        res.status(200).send("movie deleted successfully");
+   }catch(error){
+        console.log(error);
+        res.status(500).send("Error while deleting mavie");
+   }
 })
 module.exports = router ;
