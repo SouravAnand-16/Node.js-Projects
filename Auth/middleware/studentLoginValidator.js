be1bbe59a02cebf313d548ed55246d51c58815e8
Sourav Anand <sauravanand324@gmail.com>
2024-02-08 14:43:49 +0530

    password converted and coampare password done using bcrypt

--- Auth/middleware/studentLoginValidator.js
+++ Auth/middleware/studentLoginValidator.js
@@ -7,13 +7,28 @@ const studentLoginValidator = async(req,res,next)=>{
         console.log(`Email is : ${email} and password is : ${pass}`);
         const student = await StudentModel.findOne({email});
         if(!student){
-            res.status(500).send(`User doesn't exists with Email : ${email}. Please do Signup..!`);
+            res.status(404).send(`User doesn't exists with Email : ${email}. Please do Signup..!`);
         }else{
-            
+            const hash = await comparePassword(pass,student.pass);
+            console.log(hash);
+            if(hash){
+                    next() ;
+            }else{
+                res.status(401).send({"message":"Sorry wrong username of password"});
+            }
         }    
    }catch(error){
        res.status(500).send("Error while logging..");
    }
+}
+
+const comparePassword =async(pass,hash)=>{
+        try{
+            const result = await bcrypt.compare(pass,hash);
+            return result ;
+        }catch(error){
+            console.error("Error comparing passwords:", error);
+        }
 }
 
 module.exports = studentLoginValidator ;
