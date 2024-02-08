05a35d526783abe226bac05363eee92d1933b9f8
Sourav Anand <sauravanand324@gmail.com>
2024-02-08 15:31:16 +0530

    teacher bcrypt done while registering

--- Auth/Routes/teacherRoute.js
+++ Auth/Routes/teacherRoute.js
@@ -17,10 +17,6 @@ teacherRouter.get("/",async(req,res)=>{
 
 teacherRouter.post("/register",teacherValidator,async(req,res)=>{
     try{
-        const payload = req.body ;
-        console.log(payload);
-        const teacher = new TeacherModel(payload);
-        await teacher.save();
         res.status(200).send("Registration successful");
     }catch(error){
        res.status(500).send("Error while registering");
--- Auth/middleware/teacherValidator.js
+++ Auth/middleware/teacherValidator.js
@@ -1,12 +1,22 @@
 const TeacherModel = require("../model/teacherModel");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 5 ;
 const teacherValidator = async(req,res,next)=>{
   try{
-       const {email} = req.body ;
-       console.log(email);
+       const {email,pass,...payload} = req.body ;
+       const payloads = JSON.stringify(payload);
+       console.log(`Email is : ${email} and password is : ${pass} and payload is : ${payloads}`);
        const teacher = await TeacherModel.findOne({email});
-       console.log(teacher);
+       console.log("Got existing teacher from DB:",teacher);
+       console.log("calling password convertor");
+       const hashedPassword = await passConverter(pass);
+       console.log("Got hashed password : ");
        if(!teacher){
-        console.log("calling next");
+          const teacher = new TeacherModel({...req.body,pass:hashedPassword});
+          await teacher.save();
+          console.log("Saved new teacher into DB");
+          console.log("calling next");
           next();
        }else{
            res.status(500).send(`Already exist with ${email}, Please do login in..`);
@@ -16,4 +26,13 @@ const teacherValidator = async(req,res,next)=>{
   }
 }
 
-module.exports = teacherValidator ;
+const passConverter = async(pass)=>{
+        try{
+            const hash = bcrypt.hashSync(pass,saltRounds);
+            console.log("Hashed password is : ",hash);
+            return hash ;
+        }catch(error){
+             console.log(error);
+        }
+}
+module.exports = teacherValidator ;        
